de13a1926c5830c62fa2d9af7f7928f59f5d1116
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-04-07 22:30:58 +0300

    add css and hmtl files

---
+++ app.js
@@ -0,0 +1,11 @@
+const optionContainer = document.querySelector('.option-container')
+const flipButton = document.querySelector('#flip-button')
+
+let angle = 0;
+function flip() {
+    const optionShips=Array.from(optionContainer.children)
+    angle = angle === 90  ? 0 : 90;
+    optionShips.forEach(optionShip=> optionShip.style.transform = `rotate(${angle}deg)`)
+}
+
+flipButton.addEventListener('click', flip)
