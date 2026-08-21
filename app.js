2e8481ab2abd8ef94337a920a96207e025174f13
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-08-21 22:26:14 +0300

    creating ships class
    File: app.js
    
    creating block
    
    File: style.css

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// @ts-check
+
 
 const gamesBoardContainer = document.querySelector('#gamesboard-container')
 const optionContainer = document.querySelector('.option-container')
@@ -10,14 +10,38 @@ function flip() {
     angle = angle === 0 ? 90 : 0
     optionShips.forEach(optionShip => optionShip.style.transform = `rotate(${angle}deg)`)
 }
+flipButton.addEventListener('click', flip)
 
 const width = 10
-function createBoard() {
+function createBoard(color, user) {
     const gameBoardContainer = document.createElement('div')
     gameBoardContainer.classList.add('game-board')
-    gameBoardContainer.style.backgroundColor = 'pink'
+    gameBoardContainer.style.backgroundColor = color
+    gameBoardContainer.id = user
+    for(let i = 0; i < width * width; i++){
+        const block = document.createElement('div')
+        block.classList.add('block')
+        block.id = i
+        gameBoardContainer.append(block)
+    }
     gamesBoardContainer.append(gameBoardContainer)
 }
-createBoard()
+createBoard('yellow', 'player')
+createBoard('pink', 'computer')
 
-flipButton.addEventListener('click', flip)
+// Creating Ships
+class Ship {
+    constructor(name, length){
+        this.name = name;
+        this.length = length;
+    }
+}
+
+const destroyer = new Ship("destroyer", 2)
+const submarine = new Ship("cruiser", 3)
+const cruiser = new Ship("cruiser", 3)
+const battleship = new Ship("battleship", 4)
+const carrier = new Ship("carrier", 5)
+const ships = [destroyer, submarine, cruiser, battleship, carrier]
+console.log(ships)
+
