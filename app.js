9b4adb985982339ea3a2d3b521e0ac04a292a0ab
Kovalyov Ruslan <i-am-kovalyov-ruslan@yandex.ru>
2026-08-26 22:35:41 +0300

    add winner variable

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const optionContainer = document.querySelector('.option-container')
 const flipButton = document.querySelector("#flip-button")
 
 let angle = 0
+winner = true
 function flip() {
     const optionShips = Array.from(optionContainer.children);
     angle = angle === 0 ? 90 : 0
